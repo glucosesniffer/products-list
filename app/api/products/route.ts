2fdd31388c266feea7fce27bbb6e17a466a1030d
ahmad <ahmadsaeed3290@gmail.com>
2025-12-27 18:26:52 +0500

    update: move data in api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,4 +1,4 @@
-import { products } from "../../data/route";
+import { products } from "../data/route";
 
 export async function GET(request: Request) {
   return new Response(JSON.stringify(products), {
